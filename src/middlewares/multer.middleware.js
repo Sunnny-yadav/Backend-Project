fb87a8396699134d9dcb5089ae408bffd7474835
sunny-yadav <sunny9405683@gmail.com>
2024-08-06 22:21:33 +0530

    spelling updated in function

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,7 +1,7 @@
 import multer from 'multer'
 
 const storage = multer.diskStorage({
-    destination : function (req, file, callback){
+    destination : function (req, file, cb){
         callback(null,'./public/temp')
     },
     filename : function (req, file, cb){
